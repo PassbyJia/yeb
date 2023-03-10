43147f8cd1b8ca62e5fe5bb5cf46a2008014bd9f
PassbyJia <leibin2333@163.com>
2023-03-10 15:43:44 +0800

    权限组管理

---
+++ yeb-server/src/main/java/com/rainfir/server/controller/PermissionController.java
@@ -0,0 +1,50 @@
+package com.rainfir.server.controller;
+
+import com.rainfir.server.pojo.RespBean;
+import com.rainfir.server.pojo.Role;
+import com.rainfir.server.service.IRoleService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 权限组
+ *
+ */
+@RestController
+@RequestMapping("/system/basic/permiss")
+public class PermissionController {
+
+    @Autowired
+    private IRoleService roleService;
+
+    @ApiOperation(value = "获取所有角色")
+    @GetMapping("/")
+    public List<Role> getAllRoles(){
+        return roleService.list();
+    }
+
+    @ApiOperation(value = "添加角色")
+    @PostMapping("/")
+    public RespBean addRole(@RequestBody Role role){
+        //由于SpringSecuritt只认角色名前面为ROLE_的,所有自动给加上
+        if (!role.getName().startsWith("ROLE_")){
+            role.setName("ROLE_"+role.getName());
+        }
+        if (roleService.save(role)){
+            return RespBean.success("添加成功！");
+        }
+        return RespBean.error("添加失败！");
+    }
+
+    @ApiOperation(value = "删除角色")
+    @DeleteMapping("/role/{rid}")
+    public RespBean deleteRole(@PathVariable Integer rid){
+        if (roleService.removeById(rid)){
+            return RespBean.success("删除成功！");
+        }
+        return RespBean.error("删除失败！");
+    }
+}
